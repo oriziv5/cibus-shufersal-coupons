ede3b8ad36a8edaa65e68c9f50a3ad83c2f5f6b3
Ori Ziv <orziv@Oris-MacBook-Pro.local>
2021-06-04 12:16:06 +0300

    fixes

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -4,8 +4,8 @@ export const PREFIX = "בשובר ";
 /**
  * This regex capture the price, link and expiry from a valid coupon message.
  */
-export const captureRegex = /בשובר\s(?<brand>שופרסל|Be|ויקטורי)|(?<price>₪\d{2,3})|(?<link>https:\/\/www.mysodexo.co.il\/b\?\w{16,20})|(?<expire>\d{2}\/\d{2}\/\d{4})/g;
-export const captureImportRegex = /(?<brand>שופרסל|Be|ויקטורי)|(?<id>\d{16,20})|(?<price>₪\d{2,3})|(?<link>\w{16,20})|(?<expire>\d{2}\/\d{2}\/\d{4})|(?<used>true|false)/g;
+export const captureRegex = /בשובר\s(?<brand>שופרסל|Be|ויקטורי)|(?<price>₪\d{2,3})|(?<link>https:\/\/www.mysodexo.co.il\/b\?\w{16,20})|(?<expire>\d{1,2}\/\d{1,2}\/\d{4})/g;
+export const captureImportRegex = /(?<brand>שופרסל|Be|ויקטורי)|(?<id>\d{16,20})|(?<price>₪\d{2,3})|(?<link>\w{16,20})|(?<expire>\d{1,2}\/\d{1,2}\/\d{4})|(?<used>true|false)/g;
 /**
  * Regex to capture Shufersal coupon id
  */
@@ -15,7 +15,7 @@ export const cidRegex = /\d{20,22}/;
  * @todo improve with capture hebrew chars only.
  * @todo be able to read multiple messages from whatsapp.
  */
-export const shufesalMessageRegex = /.+\₪\d{2,3}.+https:\/\/www\.mysodexo\.co\.il\/b\?\w{16,20}\s.+\d{2}\/\d{2}\/\d{4}$/gm;
-export const importMessageRegex = /(Be|ויקטורי|שופרסל)\,(\d+)\,(\₪\d{1,3})\,(\d{2}\/\d{2}\/\d{4})\,(\w+)\,(true|false)$/gm;
+export const shufesalMessageRegex = /.+\₪\d{2,3}.+https:\/\/www\.mysodexo\.co\.il\/b\?\w{16,20}\s.+\d{1,2}\/\d{1,2}\/\d{4}$/gm;
+export const importMessageRegex = /(Be|ויקטורי|שופרסל)\,(\d+)\,(\₪\d{1,3})\,(\d{1,2}\/\d{1,2}\/\d{4})\,(\w+)\,(true|false)$/gm;
 
 export const onlyHebrewPattern = /^[\u0590-\u05FF\s,.:'-]+$/i;
